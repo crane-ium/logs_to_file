71d48a72fee5298c41618a04f9c9546c29467288
Crane-ium <cranegames@gmail.com>
2018-08-03 17:55:34 -0700

    File controller object mosty complete, fix the redundant writing

--- file_control/file_controller.py
+++ file_control/file_controller.py
@@ -10,11 +10,39 @@ class FileController(object):
     """
 
     def __init__(self, name):
-        pass
-    
-    def __enter__(self):
-        pass
+        self.name = name
 
-    def __exit__(self):
-        pass
+    def append(self, string):
+        self.file_data.append(string)
+    
+    def clear(self):
+        self.file_data = ''
+
+    def rewrite(self, index, string):
+        """Rewrites the index line"""
+        self.file_data[index] = string
+
+    @property
+    def len(self): 
+        return len(self.file_data)
+
+    def __str__(self):
+        return '\n'.join(self.file_data)
+    
+    @classmethod
+    def make_empty(cls, name):
+        """Returns an obj with an empty file"""
+        obj = cls(name)
+        obj.my_file = open(name, 'w')
+        obj.my_file.write('')
+        obj.my_file.close()
+        return obj 
+
+    def __enter__(self):
+        self.my_file = open(self.name, 'r+')
+        self.file_data = self.my_file.read().split('\n')
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        self.my_file.write(str(self))
+        self.my_file.close()
     
--- run.py
+++ run.py
@@ -1,10 +1,14 @@
+import random
 from file_control.file_controller import FileController
 
 class Run(object):
     def go(self):
-        pass
+        the_file = FileController("file-controlled.txt")
+        with the_file:
+            the_file.append(f"test 5")
+        print(the_file)
+
 
 if __name__ == "__main__":
     run = Run()
-    print(repr(run))
     run.go()
